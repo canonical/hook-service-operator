8cc3eff0be4b707089bdbc83437dbde79c2d6249
Nikos <nikos.sklikas@canonical.com>
2025-06-27 13:40:01 +0200

    chore: add terraform plan

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
+  description = "The Juju application name"
+  value       = juju_application.application.name
+}
+
+output "requires" {
+  description = "The Juju integrations that the charm requires"
+  value = {
+    logging = "logging"
+    tracing = "tracing"
+  }
+}
+
+output "provides" {
+  description = "The Juju integrations that the charm provides"
+  value = {
+    hydra-token-hook  = "hydra-token-hook"
+    metrics-endpoint  = "metrics-endpoint"
+    grafana-dashboard = "grafana-dashboard"
+  }
+}
