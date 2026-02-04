29c650489dd9abfbd38ddf64736a2829da39d984
Nikos Sklikas <nikos.sklikas@canonical.com>
2026-02-04 18:23:30 +0200

    chore: update tf plan

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,11 +9,13 @@ output "app_name" {
 output "requires" {
   description = "The Juju integrations that the charm requires"
   value = {
-    logging        = "logging"
-    tracing        = "tracing"
-    openfga        = "openfga"
-    pg-database    = "pg-database"
-    internal-route = "internal-route"
+    logging         = "logging"
+    tracing         = "tracing"
+    openfga         = "openfga"
+    oauth           = "oauth"
+    receive-ca-cert = "receive-ca-cert"
+    pg-database     = "pg-database"
+    internal-route  = "internal-route"
   }
 }
 
